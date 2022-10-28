82f5c2036adf4823ff5102e0db98bb0b3714c6c4
Punit Naik <naik.punit44@gmail.com>
2022-10-29 03:28:57 +0530

    re-arranged deps

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/punit-naik/sql-batcher"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/java.jdbc "0.7.12"]
-                 [com.taoensso/timbre "5.2.1"]]
+  :dependencies [[com.taoensso/timbre "5.2.1"]
+                 [org.clojars.punit-naik/in-memory-sql "0.1.0"]
+                 [org.clojure/clojure "1.11.1"]
+                 [org.clojure/java.jdbc "0.7.12"]]
   :repl-options {:init-ns org.clojars.punit-naik.sql-batcher})
