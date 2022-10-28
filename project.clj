7c7809d5eff869c54ae6b8ede35f78cfe839700f
Punit Naik <naik.punit44@gmail.com>
2022-10-28 16:28:25 +0530

    :gift: first commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject org.clojars.punit-naik/sql-batcher "0.1.0-SNAPSHOT"
+  :description "A Clojure library designed to run large sql updates in batches"
+  :url "https://github.com/punit-naik/sql-batcher"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :repl-options {:init-ns org.clojars.punit-naik.sql-batcher})
---
+++ test/org/clojars/punit_naik/sql_batcher_test.clj
@@ -0,0 +1,7 @@
+(ns org.clojars.punit-naik.sql-batcher-test
+  (:require [clojure.test :refer :all]
+            [org.clojars.punit-naik.sql-batcher :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
